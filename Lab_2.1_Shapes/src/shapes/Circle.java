e3c9303f9fa964c3907b5b3e63270dce02d2d374
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-11-08 16:10:32 -0500

    shape lab first commit

---
+++ Lab_2.1_Shapes/src/shapes/Circle.java
@@ -0,0 +1,40 @@
+package shapes;
+
+/**
+ * @author Mr Levin Created 10/16/2017 Lab 2.1 shapes
+ *
+ */
+public class Circle implements Shape {
+
+	// fields
+	private int radius;
+	private static double pi = 3.14;
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param radius
+	 * @return 
+	 */
+	public Circle(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double calculateArea() {
+		double circleArea = (pi * radius * radius);
+		return circleArea;
+	}
+
+	@Override
+	public double calculatePerimeter() {
+		double circlePerimeter = (pi * 2 * radius);
+		return circlePerimeter;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle Radius: " + radius + " Area: " + this.calculateArea() + " Perimeter: "
+				+ this.calculatePerimeter();
+	}
+}
