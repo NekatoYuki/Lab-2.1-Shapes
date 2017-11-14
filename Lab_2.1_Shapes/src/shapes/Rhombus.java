3a758ca888932c9e3ef9c380321ed72709ee97cf
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-11-14 09:25:07 -0500

    edit 12

--- Lab_2.1_Shapes/src/shapes/Rhombus.java
+++ Lab_2.1_Shapes/src/shapes/Rhombus.java
@@ -1,5 +1,35 @@
 package shapes;
 
-public class Rhombus {
+public class Rhombus implements Shape
+{
+	private int dia1;
+	private int dia2;
+	private int side;
 
+	public Rhombus(int dia1, int dia2, int side)
+	{
+		this.dia1 = dia1;
+		this.dia2 = dia2;
+		this.side = side;
+	}
+	
+	@Override
+	public double calculateArea()
+	{
+		double rhombusArea = (dia1 * dia2);
+		return rhombusArea;
+	}
+
+	public double calculatePerimeter()
+	{
+		double rhombusPerimeter = (4 * side);
+		return rhombusPerimeter;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Rhombus Diagonal 1 Length: " + dia1 + " Rhombus Diagonal 2 Length: " + dia2 + " Rhombus Side Length: " + side + " Area: " + this.calculateArea() +
+				" Perimeter: " + this.calculatePerimeter();
+	}
 }
