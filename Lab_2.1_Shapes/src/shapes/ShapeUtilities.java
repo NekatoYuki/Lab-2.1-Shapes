e22b53723c471b6dc488ca8812222e31a8f2e01b
BT_1N3_14 <BT_1N3_14@BT_1N3_14.bktech.local>
2017-11-14 09:28:24 -0500

    edit 13

--- Lab_2.1_Shapes/src/shapes/ShapeUtilities.java
+++ Lab_2.1_Shapes/src/shapes/ShapeUtilities.java
@@ -16,7 +16,7 @@ public class ShapeUtilities {
 	 */
 	public static Shape randomShape() {
 		Random rand = new Random();
-		int x = rand.nextInt(3);
+		int x = rand.nextInt(5);
 
 		switch (x) {
 		case 0:
@@ -26,9 +26,9 @@ public class ShapeUtilities {
 		case 2:
 			return new Square(rand.nextInt(50));
 		case 3:
-			return new Triangle(rand.nextInt(50));
+			return new Triangle(rand.nextInt(50), rand.nextInt(50));
 		case 4:
-			return new Rhombus(rand.nextInt(50));
+			return new Rhombus(rand.nextInt(50), rand.nextInt(50), rand.nextInt(50));
 		default:
 			return new Circle(rand.nextInt(100));
 		}
